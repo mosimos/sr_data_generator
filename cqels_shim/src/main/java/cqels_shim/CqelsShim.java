629fbe3f0a01b28acd304705e57477336ff7c0d6
Andreas Mosburger <andreas.mossburger@student.tuwien.ac.at>
2015-04-19 23:25:35 +0200

    read query from file

--- cqels_shim/src/main/java/cqels_shim/CqelsShim.java
+++ cqels_shim/src/main/java/cqels_shim/CqelsShim.java
@@ -23,6 +23,14 @@ import org.deri.cqels.data.Mapping;
 import com.hp.hpl.jena.sparql.core.Var;
 
 import java.util.Iterator;
+import java.util.List;
+import java.lang.StringBuilder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+
 
 import cqels_shim.StdinStream;
 
@@ -36,31 +44,41 @@ public class CqelsShim
 		String path = args[1];
 		String querypath = args[2];
 
+		Path qpath = Paths.get(querypath);
+		List<String> lines;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			lines = Files.readAllLines(qpath, StandardCharsets.UTF_8);
+
+			for (String s : lines) {
+				sb.append(s).append(" ");
+			}
+		} catch (IOException e) {
+			System.exit(-1);
+		}
+
+		String queryString = sb.toString();
+
+		//TODO needed?
+		//queryString = queryString.replace('\n', '');
+		
+		//String queryString = "PREFIX ns1: <http://kr.tuwien.ac.at/dhsr/> " +
+			//"SELECT ?stop ?name " +
+			//"FROM NAMED <http://kr.tuwien.ac.at/dhsr/> " +
+			//"WHERE { " +
+			//"STREAM <http://kr.tuwien.ac.at/dhsr/stream> [NOW] " +
+			//"{?stop ns1:hasName ?name} }";
+
+
 		final ExecContext context = new ExecContext(home, false);
 
-		//TODO figure out how loading this should work
-		//context.loadDataset("http://deri.org/floorplan/", "../cqels_data/floorplan.rdf");
-		context.loadDefaultDataset(path);
+		context.loadDataset("http://kr.tuwien.ac.at/dhsr/", path);
 
 		//initialize stream
-		StdinStream stream = new StdinStream(context, "http://deri.org/streams/rfid");
+		StdinStream stream = new StdinStream(context, "http://kr.tuwien.ac.at/dhsr/stream");
 
 		//register query
-		//TODO read query from file
-		String queryString = "PREFIX lv: <http://deri.org/floorplan/> " +
-			"PREFIX dc: <http://purl.org/dc/elements/1.1/> " +
-			"PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
-			"SELECT ?locName "  +
-			"FROM NAMED <http://deri.org/floorplan/> " +
-			"WHERE { " +
-			"STREAM <http://deri.org/streams/rfid> [NOW] "  +
-			"{?person lv:detectedAt ?loc} "  +
-			//"{?person foaf:name \"AUTHORNAME\"^^<http://www.w3.org/2001/XMLSchema#string> } " +
-			"GRAPH <http://deri.org/floorplan/> "  +
-			"{?loc lv:name ?locName} " +
-			"}";
-		//String queryString = "PREFIX lv: <http://deri.org/floorplan/> SELECT  ?person1 ?person2 FROM NAMED <http://deri.org/floorplan/> WHERE { GRAPH <http://deri.org/floorplan/>  {?loc1 lv:connected ?loc2} STREAM <http://deri.org/streams/rfid> [NOW] {?person1 lv:detectedAt ?loc1} STREAM <http://deri.org/streams/rfid> [RANGE 3s] {?person2 lv:detectedAt ?loc2} }";
-
 		ContinuousSelect selQuery = context.registerSelect(queryString);
 		selQuery.register(new ContinuousListener() {
 			public void update(Mapping mapping) {
